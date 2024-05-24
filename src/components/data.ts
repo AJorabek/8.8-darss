d4b3c41d01c2a6eea951062cf153fb003854f020
Abdurahmonov Jo'rabek <139786332+AJorabek@users.noreply.github.com>
2024-05-24 15:23:19 +0500

    Update data.ts

--- src/components/data.ts
+++ src/components/data.ts
@@ -1,13 +1,13 @@
 export default [
-  { code: "USD", value: 11.0 }, // Base currency
-  { code: "EUR", value: 30.85 },
+  { code: "USD", value: 105.0 }, // Base currency
+  { code: "EUR", value: 100.85 },
   { code: "JPY", value: 109.65 },
-  { code: "GBP", value: 30.75 },
-  { code: "AUD", value: 71.35 },
-  { code: "CAD", value: 21.25 },
-  { code: "CHF", value: 80.92 },
-  { code: "CNY", value: 16.47 },
-  { code: "SEK", value: 98.63 },
+  { code: "GBP", value: 110.75 },
+  { code: "AUD", value: 120.35 },
+  { code: "CAD", value: 110.25 },
+  { code: "CHF", value: 101.92 },
+  { code: "CNY", value: 106.47 },
+  { code: "SEK", value: 108.63 },
   { code: "NZD", value: 21.42 },
   { code: "ZAR", value: 60.42 },
   { code: "TRY", value: 58.42 },
